d0ec9a2a69dec3ea1c2b241d406ad32bb743d9ea
Frontend@areakart.com <frontend@areakart.com>
2026-04-07 10:06:57 +0530

    Initial commit

---
+++ dist/index.mjs
@@ -0,0 +1,7 @@
+// src/index.tsx
+import { jsx } from "react/jsx-runtime";
+var TestComponent = () => /* @__PURE__ */ jsx("div", { children: "Hello" });
+export {
+  TestComponent
+};
+//# sourceMappingURL=index.mjs.map
